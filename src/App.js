b5da13929eb379405754f3d8bd2a3d128919fb67
Jaeho Sim <simjh0919@gmail.com>
2023-12-05 14:08:55 -0500

    gh-page dependency

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt } from '@fortawesome/fontawesome-free-solid';
 import { entercom, tribalscale, overbond, ubix } from './history';
+import 'typeface-roboto';
 import './App.scss';
 
 function App() {
